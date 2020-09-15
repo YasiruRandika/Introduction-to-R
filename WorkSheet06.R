214f6d000a7aea04181d250ef0d81c73b4528435
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-09-15 09:04:03 +0530

    Update WorkSheet06.R

--- WorkSheet06.R
+++ WorkSheet06.R
@@ -1,22 +1,30 @@
-dataSet <- read.table("Data.txt", header = TRUE, sep = ",")
+dataSet <- read.table("DataSet.txt", header = TRUE, sep = ",")
 dataSet
- 
+
+#Assign the vector elements as the header names of the data set
 names(dataSet) <- c("X1", "X2")
 
 attach(dataSet)
 
+dataSet
+
 #Draw histogram
 hist(X2, main = "Histogram", xlab = "X2", ylab = "Frequency")
+abline(h = 0)
 
 #Draw a histogram using seven classes where the lower limit is 130 and an upper limit of 270
-histogram <- hist(X2, main = "Histogram", xlab = "X2", ylab = "Frequency", breaks = seq(130, 270, length=8), right = FALSE)
+#right = TRUE -> logical; if TRUE, the histogram cells are right-closed (left open) intervals. 
+histogram <- hist(X2, main = "Histogram", xlab = "X2", ylab = "Frequency", breaks = seq(130, 270, length=8), right = TRUE) #(10, 12]  10 < x <= 12
+histogram <- hist(X2, main = "Histogram", xlab = "X2", ylab = "Frequency", breaks = seq(130, 270, length=8), right = FALSE) #[10, 12)  10 ≤ x < 12
 
 #Construct the frequency distribution for the above specification.
-breaks <- round(histogram$breaks)
+breaks <- histogram$breaks
+#breaks <- round(histogram$breaks)
 freq <- histogram$counts
 mids <- histogram$mids
 
 classes <- c()
+
 
 for(i in 1:length(breaks) - 1) {
   classes[i] <- paste0("[",breaks[i],",",breaks[i+1],")")
@@ -50,4 +58,4 @@ cbind(upper = breaks, CumFreq = new)
 
 
 #. Based on the polygon, three out of four (75%) of the companies have how many shareholders or less?
-  abline(h = sum(freq) * 0.75, v=207)
+abline(h = sum(freq) * 0.75, v=207)
