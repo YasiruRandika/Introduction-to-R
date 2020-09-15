b945fb37cb694af0594c9ae4a70b1e8aedc0bac6
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-09-15 09:59:13 +0530

    Update WorkSheet06.R

--- WorkSheet06.R
+++ WorkSheet06.R
@@ -25,22 +25,27 @@ mids <- histogram$mids
 
 classes <- c()
 
-
+#paste concatenates two strings with a space character between them. paste0 does the same thing except there's no space character between the two strings being concatenated. 
 for(i in 1:length(breaks) - 1) {
   classes[i] <- paste0("[",breaks[i],",",breaks[i+1],")")
 }
 
+# cbind() to join two sets of columns together into a single dataframe.
 cbind(Classes = classes, frequency = freq)
 
-
+#Using "lines" command you are able to draw the frequency polygon in same plot 
 #Portray the distribution in the form of a frequency polygon
 lines(histogram$mids, freq)
 
 #frequency Polygon
+#type -> line
 plot(mids, freq, type = 'l', main = "Frequency polygon", xlab = "Shareholders", ylab = "Frequency", ylim = c(0, max(freq)))
+#type -> plots
+plot(mids, freq, type = 'p', main = "Frequency polygon", xlab = "Shareholders", ylab = "Frequency", ylim = c(0, max(freq)))
 
 #Portray the distribution in a less-than cumulative frequency polygon.
 cum.freq <- cumsum(freq)
+cum.freq
 
 new <-c()
 for(i in 1:length(breaks)) {
@@ -56,6 +61,13 @@ plot(breaks, new, type = 'o', main="Frequency Polygon", xlab = "Shareholders", y
 #To get the cumulative frequency table as the output, you can use the “cbind()” command.
 cbind(upper = breaks, CumFreq = new)
 
+#abline(a=NULL, b=NULL, h=NULL, v=NULL, …) 
+# a, b: single values that specify the intercept and slope of the line 
+# h: the y-value for the horizontal line 
+# v: the x-value for the vertical line
 
 #. Based on the polygon, three out of four (75%) of the companies have how many shareholders or less?
 abline(h = sum(freq) * 0.75, v=207)
+
+#detach dataSet
+detach(dataSet)
